bd8d9f8466382b637a640ec2ec11caccdecafc41
Pieter Heyvaert <pheyvaer.heyvaert@ugent.be>
2023-04-27 11:23:52 +0200

    Nicer table for history

--- src/history/script.js
+++ src/history/script.js
@@ -14,17 +14,33 @@ window.onload = () => {
 };
 
 function showHistory(history) {
-  const $tbody = document.querySelector('#table tbody');
-
-  $tbody.innerHTML = '';
-
+  // const $tbody = document.querySelector('#table tbody');
+  //
+  // $tbody.innerHTML = '';
+  //
+  // history.forEach(request => {
+  //   const {date, url, method, webId} = request;
+  //   const $tr = document.createElement('tr');
+  //   $tr.innerHTML += `<td>${date}</td>`;
+  //   $tr.innerHTML += `<td><a href="${url}">${url}</a></td>`;
+  //   $tr.innerHTML += `<td>${method}</td>`;
+  //   $tr.innerHTML += `<td><a href="${webId}">${webId}</a></td>`;
+  //   $tbody.appendChild($tr);
+  // });
   history.forEach(request => {
-    const {date, url, method, webId} = request;
-    const $tr = document.createElement('tr');
-    $tr.innerHTML += `<td>${date}</td>`;
-    $tr.innerHTML += `<td><a href="${url}">${url}</a></td>`;
-    $tr.innerHTML += `<td>${method}</td>`;
-    $tr.innerHTML += `<td><a href="${webId}">${webId}</a></td>`;
-    $tbody.appendChild($tr);
+    request.date = request.date.toISOString();
   });
+
+  new window.gridjs.Grid({
+    columns: [{
+      id: 'date', name: 'Date', sort: true, resizable: true,
+    },{
+      id: 'url', name: 'URL', sort: true, width: '750px'
+    }, {
+      id: 'method', name: 'Method', sort: true, width: '130px'
+    }, {
+      id: 'webId', name: 'WebID', sort: true, resizable: true,
+    }], data: history,
+    search: true
+  }).render(document.getElementById("wrapper"));
 }
